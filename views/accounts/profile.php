dc91a5088469a2a5f80469ef919df3268411fc35
biakaveron <biakaveron@gmail.com>
2011-11-12 00:07:30 +0400

    showing avatar at user profile page

--- views/accounts/profile.php
+++ views/accounts/profile.php
@@ -1,6 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');?>
 <h2><?php echo $user->username ?></h2>
 <ul>
+	<img src="<?php echo $user->get_avatar(80) ?>" width='80' />
 	<li>email: <?php echo $auth_data->email ?></li>
 	<li>provider: <?php echo $auth_data->service_type ?></li>
 	<li>user login: <?php echo $auth_data->service_name ?></li>
